c8cf9700a37d193ca1f82f3c9860b1e597cccc74
Charlotte Jewer <charlotte.jewer@switchconcepts.com>
2018-09-14 14:18:25 +0100

    Removed exercise notes and updated with my own annotations

--- exerciseTwo/handler.go
+++ exerciseTwo/handler.go
@@ -6,16 +6,13 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
+// MapHandler takes a map of path/urls and if the requesting URL path contains the map path, it will redirect to the url in the map
+// if no map path matches the request url path, it will deliver the fallback handler (hello() called via defaultMux())
+// which displays Hello, World!
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
 	return func(w http.ResponseWriter, r *http.Request) {
-		redirectURL, ok := pathsToUrls[r.URL.String()]
+		redirectURL, ok := pathsToUrls[r.URL.Path]
 		if ok {
 			http.Redirect(w, r, redirectURL, 301)
 		} else {
@@ -24,25 +21,10 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
+// YAMLHandler takes a yaml of path/url is mapped to the format required by MapHandler, then passed into MapHandler
+// Or if the path is not in the yaml, make MapHandler call itself
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	// parse the yaml
+
 	type config []struct {
 		Path string `yaml:"path"`
 		URL  string `yaml:"url"`
